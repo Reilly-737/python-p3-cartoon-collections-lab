7a8a09577b445a5cefebd2e7edf5b5ebb33d5859
+ <+>
2023-09-18 13:32:59 -0600

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, name in enumerate(dwarves, start=1):
+        print(f"{i}. {name}")
+    
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
+    
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+    
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheese_types:
+            return snack
+    return None
+    
